32a337e872415dc30d4e2e8ab5cd523bd17b083e
SAMIT KUMAR <kumarsamit0325@gmail.com>
2026-09-13 15:27:46 +0530

    Time: 10 ms (43.25%), Space: 31.7 MB (70.73%) - LeetHub - Time: O(n*m), Space: O(n*m)

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,51 @@
+class Solution {
+private:
+    void bfs(queue<pair<int,int>>&q, vector<vector<int>>& visited ,vector<vector<int>>&grid){
+        int n = grid.size();
+        int m = grid[0].size();
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            int delrow[] = {-1,0,+1,0};
+            int delcol[] = {0,+1,0,-1};
+            for(int i=0; i<4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow >=0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1 && !visited[nrow][ncol]){
+                    visited[nrow][ncol] = 1;
+                    q.push({nrow,ncol});
+                }
+            }
+        }
+    }
+public:
+    int numEnclaves(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        queue<pair<int,int>>q;
+        for(int row = 0; row<n ; row++){
+            for(int col =0; col<m ; col++){
+                if((row == 0 || row == n-1 || col ==0 || col == m-1) && !visited[row][col] && grid[row][col] == 1){
+                    visited[row][col] = 1;
+                    q.push({row,col});
+                }
+            }
+        }
+        bfs(q,visited,grid);
+        int count = 0;
+        for(int row =0 ; row<n ; row++){
+            for(int col =0 ;col<m ;col++){
+                if(grid[row][col] == 1 && !visited[row][col]){
+                    count ++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
